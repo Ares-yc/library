64adb502c6a722bd58721348d607066b745bda0f
颜崔 <237387718@qq.com>
2018-06-20 10:16:50 +0800

    初始化第一版

---
+++ res/src/main/java/com/ares/library/res/ResTest.java
@@ -0,0 +1,9 @@
+package com.ares.library.res;
+
+import android.util.Log;
+
+public class ResTest {
+    public static void test(){
+        Log.e("*****","This is Res test!");
+    }
+}
---
+++ ui/src/main/java/com/ares/library/ui/UITest.java
@@ -0,0 +1,9 @@
+package com.ares.library.ui;
+
+import android.util.Log;
+
+public class UITest {
+    public static void test(){
+        Log.e("*****","This is UI test!");
+    }
+}
