4f165963862ecdb523bb50385879ee5b24513a9e
颜崔 <237387718@qq.com>
2018-06-20 11:17:32 +0800

    初始化第一版修改

--- res/src/main/java/com/ares/library/res/ResTest.java
+++ res/src/main/java/com/ares/library/res/ResTest.java
@@ -2,6 +2,15 @@ package com.ares.library.res;
 
 import android.util.Log;
 
+/**
+ * =========================================
+ * Author       ：https://github.com/Ares-yc
+ * Description  ：Test the Res library
+ * VersionCode  ：1.0.0
+ * CreateDate   ：2018/6/20.
+ * UpdateDate   ：2018/6/20.
+ * =========================================
+ */
 public class ResTest {
     public static void test(){
         Log.e("*****","This is Res test!");
--- ui/src/main/java/com/ares/library/ui/UITest.java
+++ ui/src/main/java/com/ares/library/ui/UITest.java
@@ -2,6 +2,15 @@ package com.ares.library.ui;
 
 import android.util.Log;
 
+/**
+ * =========================================
+ * Author       ：https://github.com/Ares-yc
+ * Description  ：Test the UI library
+ * VersionCode  ：1.0.0
+ * CreateDate   ：2018/6/20.
+ * UpdateDate   ：2018/6/20.
+ * =========================================
+ */
 public class UITest {
     public static void test(){
         Log.e("*****","This is UI test!");
