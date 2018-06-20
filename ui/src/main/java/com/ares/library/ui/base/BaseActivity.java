255791b4c19843b2b7ca4b8f8f1ff4dc105e8c33
颜崔 <237387718@qq.com>
2018-06-20 18:18:22 +0800

    初始化第一版完成

---
+++ ui/src/main/java/com/ares/library/ui/base/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.ares.library.ui.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * =========================================
+ * Author       ：https://github.com/Ares-yc
+ * Description  ：The base class of Activity
+ * VersionCode  ：1.0.0
+ * CreateDate   ：2018/6/20.
+ * UpdateDate   ：2018/6/20.
+ * =========================================
+ */
+public class BaseActivity extends AppCompatActivity {
+
+}
---
+++ ui/src/main/java/com/ares/library/ui/base/BaseFragment.java
@@ -0,0 +1,16 @@
+package com.ares.library.ui.base;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * =========================================
+ * Author       ：https://github.com/Ares-yc
+ * Description  ：The base class of fragment
+ * VersionCode  ：1.0.0
+ * CreateDate   ：2018/6/20.
+ * UpdateDate   ：2018/6/20.
+ * =========================================
+ */
+public class BaseFragment extends Fragment{
+
+}
